a1e837b925affeb71b87544bd1f92beed0f42357
Carlos Eduardo Cabrera Ardila <ccabreraa@unal.edu.co>
2023-03-21 13:46:58 +0000

    Cambiando titulo y section

--- src/components/Structure/Head/Head.js
+++ src/components/Structure/Head/Head.js
@@ -1,63 +1,68 @@
 import React from "react";
-import "./Head.css"
-import imagenMenu from "../../../images/menu.png"
+import "./Head.css";
+import imagenMenu from "../../../images/menu.png";
 
+class Head extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mainMenu: true,
+    };
+  }
 
-class Head extends React.Component{
+  toggleMenu = () => {
+    this.setState({
+      mainMenu: !this.state.mainMenu,
+    });
+  };
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            mainMenu:true
-        }
-    }
-
-    
-    toggleMenu = () => {
-        this.setState({
-            mainMenu:!this.state.mainMenu
-        });
-    }
-
-    
-    render(){
-
-        return(
-            <div>
-                    <div class="nav-father">
-                    <header>
-                        <nav class="main-nav">
-                            <div onClick={this.toggleMenu} class="toggle-menu"> {/*Este contendor va almacenar la imagen del menu, asi
-                            mismo nos permitira delimitar el tamaño que tendra la imagen dentro del contendor.*/ }
-                                <img src={imagenMenu} alt=""/>
-                            </div>
-                            <a href="index.html" class="name"><span>Pagina profe</span></a>
-                            <div class="navv">
-                                <ul class={this.state.mainMenu ? 'main-menu':'main-menu--show'}>
-                                    <li class="main-menu_item">
-                                        <a href="Home.html" class="main-menu_link">Home</a>
-                                    </li>
-                                    <li class="main-menu_item">
-                                        <a href="about.html" class="main-menu_link">Grados</a>
-                                    </li>
-                                    <li class="main-menu_item">
-                                        <a href="Course.html" class="main-menu_link">Cursos</a>
-                                    </li>
-                                    <li class="main-menu_item">
-                                        <a href="ContacUs.html" class="main-menu_link">Calificaciones</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </nav>
-                        </header>
-                    </div>
-                
-                
-            </div>
-            
-        );
-    }
-
+  render() {
+    return (
+      <div>
+        <div class="nav-father">
+          <header>
+            <nav class="main-nav">
+              <div onClick={this.toggleMenu} class="toggle-menu">
+                {" "}
+                {/*Este contendor va almacenar la imagen del menu, asi
+                            mismo nos permitira delimitar el tamaño que tendra la imagen dentro del contendor.*/}
+                <img src={imagenMenu} alt="" />
+              </div>
+              <a href="index.html" class="name">
+                <span>Autonomous Education Web</span>
+              </a>
+              <div class="navv">
+                <ul
+                  class={this.state.mainMenu ? "main-menu" : "main-menu--show"}
+                >
+                  <li class="main-menu_item">
+                    <a href="Home.html" class="main-menu_link">
+                      Home
+                    </a>
+                  </li>
+                  <li class="main-menu_item">
+                    <a href="about.html" class="main-menu_link">
+                      Grados
+                    </a>
+                  </li>
+                  <li class="main-menu_item">
+                    <a href="Course.html" class="main-menu_link">
+                      Cursos
+                    </a>
+                  </li>
+                  <li class="main-menu_item">
+                    <a href="ContacUs.html" class="main-menu_link">
+                      Calificaciones
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </header>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Head;
